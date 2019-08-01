0c3fe0f6da7132d887bad250471ceaf36ee126a7
goseeya <149542@gmail.com>
2019-08-01 18:48:10 +0200

    Style activelinks

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -16,9 +16,16 @@ class Blog extends Component {
               <header>
                 <nav>
                   <ul>
-                    <li><NavLink to="/" exact>Home</NavLink></li>
+                    <li><NavLink
+                      to="/"
+                      exact
+                      activeClassName="my-active"
+                      activeStyle={{
+                        color: '#fa923f',
+                        textDecoration: 'underline'
+                      }}>Home</NavLink></li>
                     <li><NavLink to={{
-                      pathname: this.props.match.url + '/new-post',
+                      pathname: '/new-post',
                       hash: '#submit',
                       search: '?quick-submit=true'
                     }}>New Post</NavLink></li>
