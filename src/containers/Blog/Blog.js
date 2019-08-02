d2f0c22d133bad836ed29711053b92340c58c4e9
goseeya <149542@gmail.com>
2019-08-02 15:07:57 +0200

    Access router params

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -6,6 +6,7 @@ import { Route, NavLink, withRouter } from 'react-router-dom';
 import './Blog.css';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
 
@@ -38,6 +39,7 @@ class Blog extends Component {
                 */}
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
 
             </div>
         );
